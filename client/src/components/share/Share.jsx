92cde8a306d98128fc9fea8f923423dde83b88a0
Paulongf <paulongf@hotmail.com>
2025-03-07 17:05:05 +0000

    Adicionando frontend (client) ao repositório principal corretamente

---
+++ client/src/components/share/Share.jsx
@@ -0,0 +1,89 @@
+import "./share.scss";
+import Image from "../../assets/img.png";
+import Map from "../../assets/map.png";
+import Friend from "../../assets/friend.png";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/authContext";
+import {
+  useMutation,
+  useQueryClient,
+} from '@tanstack/react-query'
+import { makeRequest } from "../../axios";
+
+const Share = () => {
+
+  const [file, setFile] = useState(null);
+  const [description, setDescription] = useState("");
+
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await makeRequest.post("/upload", formData);
+      return res.data.filePath; 
+  
+    } catch (err) {
+      console.log(err);
+      return ""; 
+    }
+  };
+  
+
+  const {currentUser} = useContext(AuthContext);
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (newPost) => makeRequest.post("/posts", newPost),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    }
+  });
+  
+
+  const handleClick = async (e) =>{
+    e.preventDefault();
+    let imgUrl = "";
+    if(file) imgUrl = await upload();
+    mutation.mutate({description, img: imgUrl})
+  };
+
+  return (
+    <div className="share">
+      <div className="container">
+        <div className="top">
+          <img
+            src={currentUser.profilePic}
+            alt=""
+          />
+          <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} onChange={e=>setDescription(e.target.value)} />
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <input type="file" id="file" style={{display:"none"}} onChange={e=>setFile(e.target.files[0])} />
+            <label htmlFor="file">
+              <div className="item">
+                <img src={Image} alt="" />
+                <span>Add Image</span>
+              </div>
+            </label>
+            <div className="item">
+              <img src={Map} alt="" />
+              <span>Add Place</span>
+            </div>
+            <div className="item">
+              <img src={Friend} alt="" />
+              <span>Tag Friends</span>
+            </div>
+          </div>
+          <div className="right">
+            <button onClick={handleClick}>Share</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
